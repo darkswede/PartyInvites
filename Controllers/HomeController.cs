87bf4dfb7a2ade0c4206cd8b3c3f5ba2dae32cd9
Marcin Pietrucha <mpietrucha@g2a.com>
2021-02-21 17:47:02 +0100

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "good morning" : "good evening";
+            return View("MyView");
+        }
+    }
+}
