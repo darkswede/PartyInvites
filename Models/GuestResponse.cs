598ce47cb727aa25a606d6dfb34107b4e9854e3b
Marcin Pietrucha <mpietrucha@g2a.com>
2021-03-03 18:19:40 +0100

    validation form added

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -1,10 +1,18 @@
-﻿namespace PartyInvites.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "please enter Your name and surname")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "please enter Your e-mail address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "please provide a valid e-mail address")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "please enter phone number")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "please make a choice")]
         public bool? WillAttend { get; set; }
     }
 }
